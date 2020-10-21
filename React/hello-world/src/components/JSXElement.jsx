c03b0dae6559ef5a547f30d7f573c708d4c4568b
Priyobrato Das <priyodas12@gmail.com>
2020-10-21 07:46:26 +0530

    adding jsx

--- React/hello-world/src/components/JSXElement.jsx
+++ React/hello-world/src/components/JSXElement.jsx
@@ -4,7 +4,8 @@ export function Jsxelement(props) {
 
   return (
     //<h1>Hi, I am JSX element</h1>
-    React.createElement('div', { id: 'hello1', class: 'test1' }, React.createElement('h1', { id: 'hello2', class: 'test2' }, 'Hi, I am JSX element'))
+    React.createElement('div', { id: 'hello1', className: 'test1' }, React.createElement('h1', { id: 'hello2', className: 'test2' }, 'Hi, I am JSX element'))
   )
 }
+
 
